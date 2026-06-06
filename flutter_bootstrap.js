42dca243bd3535a414f30e11c354d7b852d6ffdb
Ajilesh v <59503437+Aji46@users.noreply.github.com>
2026-06-06 23:05:11 +0530

    fix website

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1067520156" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1480701643" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
